c1b01b442810886b4647785e3aea3ca23a0aba6a
Mirian Margiani <mirian@margiani.ch>
2020-11-18 22:07:43 +0100

    Add toolbar icons as SVG: copy, cut, properties, select-all

--- icon-src/render-icons.sh
+++ icon-src/render-icons.sh
@@ -37,6 +37,10 @@ echo "rendering toolbar icons..."
 
 files=(
     toolbar-rename@64
+    toolbar-copy@64
+    toolbar-cut@64
+    toolbar-properties@64
+    toolbar-select-all@64
     harbour-file-browser@86
     harbour-file-browser-root@86
     icon-btn-search@112
