a09974d5ff10811974dbabb75a183daa5084a331
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-01-07 21:52:08 +0100

    Update root build for latest SFOS

--- root/build.sh
+++ root/build.sh
@@ -3,8 +3,8 @@
 SFDK=/opt/SailfishOS-SDK/bin/sfdk
 TARGETS=(
     # note: last field (/-.*?$/) has to be architecture
-    "SailfishOS-3.3.0.16-armv7hl"
-    "SailfishOS-3.3.0.16-i486"
+    "SailfishOS-3.4.0.24-armv7hl"
+    "SailfishOS-3.4.0.24-i486"
 )
 
 showhelp() {
