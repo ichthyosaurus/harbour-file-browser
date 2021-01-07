aa40ec0695aa7ee588fda476e3286c9b9885ceb6
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-01-07 21:14:21 +0100

    Add script to prepare changelog for openrepos

---
+++ rpm/changelog-openrepos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This file is part of File Browser.
+# SPDX-FileCopyrightText: 2021 Mirian Margiani
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This script uses the converted RPM changelog (generated from CHANGELOG.md by
+# rpm/harbour-file-browser-beta.run) and converts it to be used on OpenRepos.
+#
+
+converter="./rpm/harbour-file-browser-beta.changes.run"
+
+if [[ "$(basename "$(pwd)")" == "rpm" ]]; then
+    cd ..  # run from base directory
+fi
+
+if [[ ! -f "$converter" ]]; then
+    echo "error: main conversion script not found"
+    exit 2
+fi
+
+bash "$converter" |\
+    sed -Ee 's@\* ... (... .. ....) .*?> (.*)$@</ul><p><strong>version \2: \1</strong></p><ul>@g;
+             s/^- (.*)$/<li>\1<\/li>/g;
+             /^$/d;' |\
+        sed -Ee '1s@</ul>@@g;
+                 $s@$@</ul>@g'
