e9b73b14f5eac813d3a501020d78e43eb6b7ccce
Ladislav Hodas <ladislav@hodas.sk>
2021-02-08 00:24:57 +0100

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (405 of 405 strings)
    
    Co-authored-by: Ladislav Hodas <ladislav@hodas.sk>
    Translate-URL: https://hosted.weblate.org/projects/harbour-file-browser/main-translations/sk/
    Translation: File Browser for SailfishOS/Translations

--- translations/harbour-file-browser-sk.ts
+++ translations/harbour-file-browser-sk.ts
@@ -134,17 +134,17 @@
     <message>
         <location filename="../qml/pages/ContributorsPage.qml" line="108"/>
         <source>Czech</source>
-        <translation type="unfinished"></translation>
+        <translation>Čeština</translation>
     </message>
     <message>
         <location filename="../qml/pages/ContributorsPage.qml" line="112"/>
         <source>Slovak</source>
-        <translation type="unfinished"></translation>
+        <translation>Slovenčina</translation>
     </message>
     <message>
         <location filename="../qml/pages/ContributorsPage.qml" line="116"/>
         <source>Hungarian</source>
-        <translation type="unfinished"></translation>
+        <translation>Maďarčina</translation>
     </message>
 </context>
 <context>
