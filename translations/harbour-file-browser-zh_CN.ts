65e5152ed760072da296aefe6f3e0c2975d7658d
玉堂白鹤 <yjwork@qq.com>
2021-04-13 11:24:36 +0200

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (406 of 406 strings)
    
    Co-authored-by: 玉堂白鹤 <yjwork@qq.com>
    Translate-URL: https://hosted.weblate.org/projects/harbour-file-browser/main-translations/zh_Hans/
    Translation: File Browser for SailfishOS/Translations

--- translations/harbour-file-browser-zh_CN.ts
+++ translations/harbour-file-browser-zh_CN.ts
@@ -149,7 +149,7 @@
     <message>
         <location filename="../qml/pages/ContributorsPage.qml" line="120"/>
         <source>Estonian</source>
-        <translation type="unfinished"></translation>
+        <translation>爱沙尼亚语</translation>
     </message>
 </context>
 <context>
