7f8541322cc246c9139e6cebaee827aea492583a
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2022-04-05 01:20:05 +0200

    Refactor the root mode helper build script again

--- root/build.sh
+++ root/build.sh
@@ -12,37 +12,84 @@ TARGETS=(
     "SailfishOS-3.4.0.24-armv7hl"
 )
 
+log-n() { echo -ne "\e[1m"; printf -- "%s " "$@" | sed 's/ $//g'; echo -ne "\e[0m"; }
+log() { log-n "$@" && echo; }
+
 showhelp() {
-    echo "** build harbour-file-browser-root **"
-    echo "usage: build.sh [-h|--help]"
+    log "** build harbour-file-browser-root **"
+    log "usage: build.sh [-h|--help] [-n|--no-stop-engine]"
     echo
     echo "Modify this script to set the correct paths for your setup"
     echo "by changing the SFDK and TARGETS variables."
+    echo
+    echo "Use the --no-stop-engine option to keep the build engine"
+    echo "running after the script has ended. By default, the build"
+    echo "engine is started and stopped by this script automatically."
 }
 
-if [[ "$1" =~ ^-?-h(elp)?$ ]]; then
-    showhelp
-    exit 0
-elif (( $# > 0 )); then
-    echo "error: invalid arguments"
-    showhelp
-    exit 1
+cCUSTOM_TARGETS=()
+cSTOP_ENGINE=true
+for i in "$@"; do
+    if [[ "$i" =~ ^-?-h(elp)?$ ]]; then
+        showhelp
+        exit 0
+    elif [[ "$i" == "-n" || "$i" == "--no-stop-engine" ]]; then
+        cSTOP_ENGINE=false
+        continue
+    elif [[ "$i" == "-"* ]]; then
+        log "error: unknown argument '$i'"
+        showhelp
+        exit 1
+    fi
+
+    cCUSTOM_TARGETS+=("$i")
+done
+
+if (( ${#cCUSTOM_TARGETS[@]} > 0 )); then
+    TARGETS=("${cCUSTOM_TARGETS[@]}")
 fi
 
 if [[ ! -f "$SFDK" ]]; then
-    echo "error: could not find sfdk at '$SFDK'"
+    log "error: could not find sfdk at '$SFDK'"
     exit 2
 fi
 
-echo "starting build engine..."
+log "selected targets:"
+printf -- "- %s\n" "${TARGETS[@]}"
+echo
+
+echo "starting the build engine..."
 "$SFDK" engine start
 
-mv -T --backup=t RPMS RPMS~
+mv -T --backup=t RPMS RPMS~ || true
+
+declare -A status
 
 for t in "${TARGETS[@]}"; do
-    "$SFDK" engine exec sb2 -t "$t" make BUILDARCH="${t##*-}"
+    status["$t"]="ok"
+
+    "$SFDK" engine exec sb2 -t "$t" make BUILDARCH="${t##*-}" || {
+        status["$t"]="failed"
+    }
 done
 
-echo "if you no longer need it, stop the build engine by running:"
-echo "$SFDK engine stop"
+echo; echo
+log "builds finished:"
+for t in "${!status[@]}"; do
+    echo -ne "- \e[1m${status[$t]}\t\e[0m"
+    printf -- "%s\n" "$t"
+done
+echo
+
+if [[ "$cSTOP_ENGINE" == true ]]; then
+    echo "stopping the build engine..."
+    "$SFDK" engine stop || {
+        log "error: failed to stop the build engine"
+        printf -- "%s\n" "Try running this manually: $SFDK engine stop"
+    }
+else
+    printf -- "%s\n" "if you no longer need it, stop the build engine by running:"
+    printf -- "\t%s\n" "$SFDK engine stop"
+fi
+
 exit 0
