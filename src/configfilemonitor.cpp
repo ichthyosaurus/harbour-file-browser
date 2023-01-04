e6c894391ad0c5e3a93be10ad7983296a068f78a
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2023-01-04 15:01:03 +0100

    Implement a monitor for synchronized config files

---
+++ src/configfilemonitor.cpp
@@ -0,0 +1,334 @@
+/*
+ * This file is part of File Browser.
+ * SPDX-FileCopyrightText: 2023 Mirian Margiani
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "configfilemonitor.h"
+
+#include <QDebug>
+#include <QFileSystemWatcher>
+#include <QFile>
+#include <QFileInfo>
+
+
+/***********************************************************************
+ * Private class
+ ***********************************************************************/
+
+class ConfigFileMonitorPrivate {
+    Q_DISABLE_COPY(ConfigFileMonitorPrivate)
+    Q_DECLARE_PUBLIC(ConfigFileMonitor)
+    ConfigFileMonitor* const q_ptr;
+
+public:
+    ConfigFileMonitorPrivate(ConfigFileMonitor* q);
+    ~ConfigFileMonitorPrivate();
+
+public slots:
+    void handleFilesystemEvent(bool notify = true);
+    void clearWatcherPaths();
+
+public:
+    QString m_file {};
+    QString m_parentDir {};
+    ConfigFileMonitor::ConfigFileMonitorOptions m_options {0};
+    QFileSystemWatcher m_watcher;
+
+    // QString m_lockFile {};
+    const int m_defaultMaximumFileSize {200*1024}; /* 200 KiB */;
+    const int m_absoluteMaximumFileSize {1*1024*1024}; /* 1 MiB */
+    int m_maximumFileSize {m_defaultMaximumFileSize};
+};
+
+
+/***********************************************************************
+ * Implementation
+ ***********************************************************************/
+
+ConfigFileMonitorPrivate::ConfigFileMonitorPrivate(ConfigFileMonitor* q) :
+    q_ptr(q), m_watcher(q)
+{
+    //
+}
+
+ConfigFileMonitorPrivate::~ConfigFileMonitorPrivate()
+{
+    //
+}
+
+void ConfigFileMonitorPrivate::handleFilesystemEvent(bool notify)
+{
+    qDebug() << "checking monitored config file:" << m_file
+             << ", signals:" << m_watcher.signalsBlocked()
+             << ", files:" << m_watcher.files()
+             << ", dirs:" << m_watcher.directories()
+             << ", notify:" << notify;
+
+    /* if (QFile::exists(m_lockFile)) {
+        qDebug() << "-> locked, skipping event";
+        return;
+    } */
+
+    if (QFile::exists(m_file)) {
+        // config file exists
+        // -> watch it for changes
+        // -> notify
+
+        if (!m_watcher.files().contains(m_file)) {
+            clearWatcherPaths();
+            qDebug() << "-> switching to watch file";
+
+            if (!m_watcher.addPath(m_file)) {
+                qWarning() << "failed to watch config file for changes at" << m_file;
+                qWarning() << "data might be lost when using multiple app windows";
+            }
+        }
+
+        if (notify) {
+            // notify, then wait for changes
+            emit q_ptr->configChanged();
+        }
+    } else {
+        // config file does not exist:
+        // -> watch directory to catch when the file is created
+        // -> then watch it for changes when this method gets called again
+
+        if (!m_watcher.directories().contains(m_parentDir)) {
+            clearWatcherPaths();
+            qDebug() << "-> switching to watch directory";
+
+            if (!m_watcher.addPath(m_parentDir)) {
+                qWarning() << "failed to watch config directory for changes at" << m_parentDir;
+                qWarning() << "data might be lost when using multiple app windows";
+            }
+        }
+
+        if (m_options.testFlag(ConfigFileMonitor::NotifyWhenRemoved)) {
+            if (notify) {
+                emit q_ptr->configChanged();
+            }
+        } else {
+            // abort and wait for changes, there is nothing to notify yet
+            return;
+        }
+    }
+}
+
+void ConfigFileMonitorPrivate::clearWatcherPaths()
+{
+    const auto& dirs = m_watcher.directories();
+    if (!dirs.isEmpty()) {
+        m_watcher.removePaths(dirs);
+    }
+
+    const auto& files = m_watcher.files();
+    if (!files.isEmpty()) {
+        m_watcher.removePaths(files);
+    }
+}
+
+ConfigFileMonitor::ConfigFileMonitor(QObject* parent) :
+    QObject(parent),
+    d_ptr(new ConfigFileMonitorPrivate(this))
+{
+    //
+}
+
+ConfigFileMonitor::~ConfigFileMonitor()
+{
+    //
+}
+
+void ConfigFileMonitor::reset(const QString& configFile, const ConfigFileMonitorOptions& options, int maximumSize)
+{
+    Q_D(ConfigFileMonitor);
+    auto info = QFileInfo(configFile);
+
+    if (configFile.isEmpty() || info.filePath().isEmpty()) {
+        // abort without setting up any connections
+        qWarning() << "cannot monitor invalid path" << configFile;
+        d->clearWatcherPaths();
+        return;
+    }
+
+    if (maximumSize > 0 && maximumSize < d->m_absoluteMaximumFileSize) {
+        d->m_maximumFileSize = maximumSize;
+    } else {
+        d->m_maximumFileSize = d->m_defaultMaximumFileSize;
+    }
+
+    d->m_file = info.absoluteFilePath();
+    d->m_parentDir = info.absolutePath();
+    // d->m_lockFile = info.absoluteFilePath() + ".lock";
+    d->m_options = options;
+    d->m_watcher.blockSignals(false);
+
+    d->m_watcher.disconnect();
+    connect(&d->m_watcher, &QFileSystemWatcher::fileChanged,
+            this, [&](){
+        Q_D(ConfigFileMonitor);
+        d->handleFilesystemEvent();
+    });
+    connect(&d->m_watcher, &QFileSystemWatcher::directoryChanged,
+            this, [&](){
+        Q_D(ConfigFileMonitor);
+        d->handleFilesystemEvent();
+    });
+
+    d->handleFilesystemEvent();
+}
+
+QString ConfigFileMonitor::file() const
+{
+    Q_D(const ConfigFileMonitor);
+    return d->m_file;
+}
+
+ConfigFileMonitor::ConfigFileMonitorOptions ConfigFileMonitor::options() const
+{
+    Q_D(const ConfigFileMonitor);
+    return d->m_options;
+}
+
+bool ConfigFileMonitor::isRunning() const
+{
+    Q_D(const ConfigFileMonitor);
+    return d->m_watcher.signalsBlocked();
+}
+
+QString ConfigFileMonitor::readFile() const
+{
+    Q_D(const ConfigFileMonitor);
+
+    if (d->m_file.isEmpty()) {
+        qDebug() << "cannot read file without filename";
+        return {};  // cannot read an invalid file
+    }
+
+    QFile file(d->m_file);
+
+    if (!file.exists()) {
+        qDebug() << "config file" << d->m_file << "not found";
+        return {};  // no config file found
+    }
+
+    if (!file.open(QFile::ReadOnly)) {
+        qWarning() << "cannot open config file at" << d->m_file;
+        return {};  // FIXME report, as this might actually be a problem
+    }
+
+    if (file.size() > d->m_maximumFileSize) {
+        qWarning() << "config file at" << d->m_file << "is unreasonably large:" <<
+                      file.size() / 1024 << "KiB, maximum is" << d->m_maximumFileSize / 1024 << "KiB";
+        return {};  // FIXME report, as this might actually be a problem
+    }
+
+    QTextStream stream(&file);
+    QString read = file.readAll();
+    file.close();
+
+    return read;
+}
+
+int ConfigFileMonitor::maximumFileSize() const
+{
+    Q_D(const ConfigFileMonitor);
+    return d->m_maximumFileSize;
+}
+
+void ConfigFileMonitor::pause()
+{
+    Q_D(ConfigFileMonitor);
+    qDebug() << "pausing monitor for" << d->m_file;
+    d->m_watcher.blockSignals(true);
+    d->clearWatcherPaths();
+}
+
+void ConfigFileMonitor::resume()
+{
+    Q_D(ConfigFileMonitor);
+
+    if (d->m_file.isEmpty()) {
+        qWarning() << "cannot resume without being initialized with a valid file";
+        pause();
+        return;
+    } else if (!d->m_watcher.signalsBlocked()) {
+        qDebug() << "cannot resume without being paused";
+        return;
+    }
+
+    qDebug() << "resuming monitor for" << d->m_file;
+    d->handleFilesystemEvent(false);
+    d->m_watcher.blockSignals(false);
+}
+
+void ConfigFileMonitor::setRunning(bool running)
+{
+    if (running) resume();
+    else pause();
+}
+
+/*void ConfigFileMonitor::lock()
+{
+    // TODO
+}
+
+void ConfigFileMonitor::unlock()
+{
+    // TODO
+}
+
+void ConfigFileMonitor::setLocked(bool locked)
+{
+    if (locked) lock();
+    else unlock();
+}*/
+
+
+/***********************************************************************
+ * Blocker implementation
+ ***********************************************************************/
+
+ConfigFileMonitorBlocker::ConfigFileMonitorBlocker(ConfigFileMonitor* monitor) noexcept :
+    m_monitor(monitor)
+{
+    if (m_monitor) {
+        // m_wasRunning = m_monitor->isRunning();
+        // m_monitor->lock();
+        m_monitor->pause();
+        qDebug() << "blocker started for" << m_monitor->file();
+    } else {
+        qDebug() << "blocker created for invalid monitor";
+    }
+}
+
+ConfigFileMonitorBlocker::~ConfigFileMonitorBlocker()
+{
+    if (m_monitor) {
+        // m_monitor->setLocked(m_wasLocked);
+        // m_monitor->setRunning(m_wasRunning);
+        // qDebug() << "blocker finished for" << m_monitor->file() << "| resuming, was running:" << m_wasRunning;
+
+        m_monitor->resume();
+        qDebug() << "blocker finished for" << m_monitor->file() << ", resuming";
+    } else {
+        qDebug() << "blocker finished for invalid monitor";
+    }
+}
+
+/*
+void ConfigFileMonitorBlocker::suspendBlocking() noexcept
+{
+    if (m_monitor && m_wasRunning) m_monitor->resume();
+    m_blockingSuspended = true;
+    qDebug() << "blocker suspended";
+}
+
+void ConfigFileMonitorBlocker::resumeBlocking() noexcept
+{
+    if (m_monitor) m_monitor->pause();
+    m_blockingSuspended = false;
+    qDebug() << "blocker resumed";
+}
+*/
---
+++ src/configfilemonitor.h
@@ -0,0 +1,141 @@
+/*
+ * This file is part of File Browser.
+ * SPDX-FileCopyrightText: 2023 Mirian Margiani
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef CONFIGFILEMONITOR_H
+#define CONFIGFILEMONITOR_H
+
+#include <QObject>
+#include <QPointer>
+
+class ConfigFileMonitorPrivate;
+
+/**
+ * @brief The ConfigFileMonitor class watches a file for modifications.
+ *
+ * The signal \c configChanged is emitted when the config file
+ * changes on disk. Unless the \c NotifyWhenRemoved option is set,
+ * it will not be emitted when the file is removed.
+ *
+ * @section Reading
+ *
+ * The file contents can be accessed using \c readFile, if the
+ * file is smaller than the maximum file size (default: 200 KiB).
+ *
+ * @section Writing
+ *
+ * Use the \l QSaveFile class for saving, and use the \c QIODevice::Unbuffered
+ * option when opening the save file.
+ *
+ * Always pause the monitor using \c pause before starting the
+ * save operation, and resume it afterwards using \c resume. To
+ * simplify this, you can use a \c ConfigFileMonitorBlocker
+ * that automatically resumes monitoring when it is destroyed.
+ */
+class ConfigFileMonitor : public QObject {
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(ConfigFileMonitor)
+    QScopedPointer<ConfigFileMonitorPrivate> const d_ptr;
+
+public:  // flags
+    enum ConfigFileMonitorOption {
+        NotifyWhenRemoved = 0x01
+    };
+    Q_DECLARE_FLAGS(ConfigFileMonitorOptions, ConfigFileMonitorOption)
+
+public:  // interface
+    explicit ConfigFileMonitor(QObject* parent = nullptr);
+    virtual ~ConfigFileMonitor();
+
+    void reset(const QString& configFile,
+               const ConfigFileMonitorOptions& options = 0,
+               int maximumSize = -1);
+
+    QString file() const;
+    ConfigFileMonitorOptions options() const;
+    bool isRunning() const;
+
+    QString readFile() const;
+    int maximumFileSize() const;
+
+public slots:
+    void pause();
+    void resume();
+    void setRunning(bool running);
+
+    /*void lock();
+    void unlock();
+    void setLocked(bool locked);*/
+
+signals:
+    void configChanged();
+};
+Q_DECLARE_OPERATORS_FOR_FLAGS(ConfigFileMonitor::ConfigFileMonitorOptions)
+
+
+/**
+ * @brief The ConfigFileMonitorBlocker class pauses monitoring until it is destroyed.
+ *
+ * \c ConfigFileMonitorBlocker can be used instead of a pair of calls to
+ * \c ConfigFileMonitor::pause and \c ConfigFileMonitor::resume.
+ * This ensures that the monitor is always resumed after it has been paused.
+ *
+ * Use this helper when saving config files like this:
+ *
+ * \code
+ * void save() {
+ *     ConfigFileMonitorBlocker blocker(myMonitor);
+ *
+ *     // ... save stuff ...
+ *
+ *     if (errorOccurred) {
+ *         // return without having to worry about resuming the monitor
+ *         return;
+ *     }
+ *
+ *     // ... more stuff ...
+ * }
+ * \endcode
+ *
+ * Without the blocker, you would have to manually pause and resume
+ * the monitor. This causes issues when exceptions occur during saving.
+ *
+ * \code
+ * void save() {
+ *     myMonitor->pause();
+ *
+ *     // ... save stuff ...
+ *
+ *     if (errorOccurred) {
+ *         myMonitor->resume();
+ *         return;
+ *     }
+ *
+ *     // ... more stuff ...
+ *     myMonitor->resume();
+ * }
+ * \endcode
+ */
+class ConfigFileMonitorBlocker
+{
+    Q_DISABLE_COPY(ConfigFileMonitorBlocker)
+
+public:
+    explicit ConfigFileMonitorBlocker(ConfigFileMonitor* monitor) noexcept;
+    inline explicit ConfigFileMonitorBlocker(ConfigFileMonitor& monitor) noexcept :
+        ConfigFileMonitorBlocker(&monitor) {}
+    ~ConfigFileMonitorBlocker();
+
+    // void suspendBlocking() noexcept;
+    // void resumeBlocking() noexcept;
+
+private:
+    QPointer<ConfigFileMonitor> m_monitor;
+    // bool m_wasRunning {true};
+    // bool m_wasLocked {false};
+    // bool m_blockingSuspended {false};
+};
+
+#endif  // CONFIGFILEMONITOR_H
