a3ac3849c392b0777ddf28e3b7cf5fb5eb3bbec7
Victor K <ktrace@yandex.ru>
2024-10-13 14:48:01 +0200

    Translated using Weblate (Russian)
    Currently translated at 99.6% (580 of 582 strings)
    
    Co-authored-by: Victor K <ktrace@yandex.ru>
    Translate-URL: https://hosted.weblate.org/projects/harbour-file-browser/translations/ru/
    Translation: File Browser for SailfishOS/App: File Browser

--- translations/harbour-file-browser-ru.ts
+++ translations/harbour-file-browser-ru.ts
@@ -31,12 +31,12 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="154"/>
         <source>Romanian</source>
-        <translation type="unfinished"></translation>
+        <translation>Румынский</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="169"/>
         <source>Norwegian Bokmål</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Норвежский букмол</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="238"/>
@@ -56,7 +56,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="246"/>
         <source>Dutch (Belgium)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Голландский (Бельгия)</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="265"/>
