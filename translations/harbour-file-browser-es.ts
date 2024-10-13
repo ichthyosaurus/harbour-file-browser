6139e1ee2d9c59e6b2329a5ce99f4d9b489f2fc1
gallegonovato <fran-carro@hotmail.es>
2024-10-13 14:48:01 +0200

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (582 of 582 strings)
    
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>
    Translate-URL: https://hosted.weblate.org/projects/harbour-file-browser/translations/es/
    Translation: File Browser for SailfishOS/App: File Browser

--- translations/harbour-file-browser-es.ts
+++ translations/harbour-file-browser-es.ts
@@ -31,7 +31,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="154"/>
         <source>Romanian</source>
-        <translation type="unfinished"></translation>
+        <translation>Rumano</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="169"/>
@@ -56,7 +56,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="246"/>
         <source>Dutch (Belgium)</source>
-        <translation type="unfinished"></translation>
+        <translation>Holandés (Bélgica)</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="265"/>
