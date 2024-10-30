34e557a1bec9befaa3ced05e506d6e066c5480c8
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-10-30 21:12:05 +0100

    Add wip enumcontainer with more features

---
+++ src/enumcontainer-todo.h
@@ -0,0 +1,95 @@
+/*
+ * This file is part of File Browser.
+ *
+ * SPDX-FileCopyrightText: 2022-2024 Mirian Margiani
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef ENUMCONTAINER_H
+#define ENUMCONTAINER_H
+
+#include <QtQml/QtQml>
+#include <QObject>
+
+// Use the following macros to expose an enumeration to QML.
+//
+// Usage:
+//
+// 0. Include "enumcontainer.h" in the header file.
+//
+// 1. Declare a new enum using the CREATE_ENUM macro in the header file.
+//      CREATE_ENUM(MyEnum, Value1, Value2, Value3)
+//
+// 2. Declare a new registration function (only once) in the header file.
+//      DECLARE_ENUM_REGISTRATION_FUNCTION(MyNamespace)
+//
+// 3. Define the registration function in the code file.
+//      DEFINE_ENUM_REGISTRATION_FUNCTION(MyNamespace) {
+//          REGISTER_ENUM_CONTAINER(MyEnum)
+//      }
+//
+// 4. Actually do the registration in the main() function.
+//      REGISTER_ENUMS(MyNamespace, "MyApp.MyModule", 1, 0)
+//
+// 5. Workaround: duplicate the registration in the main() function so the
+//    enums get picked up by QtCreator's completion system
+//      qmlRegisterUncreatableType<MyEnum>("MyApp.MyModule", 1, 0, "MyEnum", "This is only a container for an enumeration.");
+//
+// 6. Workaround: if your header file does not contain the Q_OBJECT macro,
+//    then qmake will not run "moc" on it and the project will not compile.
+//    Add this to the header file as a workaround:
+//      #ifdef __HACK_TO_FORCE_MOC
+//      Q_OBJECT
+//      #endif
+//
+
+
+// TODO and FIXME:
+// Calling static methods from QML is not possible, even though
+// it looks like it. You always get "TypeError: Property 'myMethod' of object [object Object] is not a function"
+// It is only possible through a singleton type, and a singleton type
+// must be a QObject, which is trouble.
+
+#define CREATE_ENUM(NAME, VALUES...) \
+    class NAME { \
+        Q_GADGET \
+    \
+    public: \
+        enum Enum { VALUES }; \
+        \
+        Q_ENUM(Enum) /* using "NAME::Enum" here would make it "undefined" in QML */ \
+        \
+        static void registerToQml(const char* url, int major, int minor) { \
+            static const char* qmlName = #NAME; \
+            qmlRegisterUncreatableType<NAME>(url, major, minor, qmlName, "This is only a container for an enumeration."); \
+        } \
+        \
+        Q_INVOKABLE static bool isValid(int value) { \
+            return !QMetaEnum::fromType<Enum>().valueToKeys(value).isEmpty(); \
+        } \
+        \
+        Q_INVOKABLE static QString string(int value) { \
+            return QString::fromLatin1(QMetaEnum::fromType<Enum>().valueToKeys(value)); \
+        } \
+        \
+        explicit NAME() { throw std::logic_error("This class is only a container for an enum."); }; \
+    }; \
+    Q_DECLARE_METATYPE(NAME)
+
+#define DECLARE_ENUM_REGISTRATION_FUNCTION(NAMESPACE) \
+    namespace NAMESPACE##Enums { void registerEnumTypes(const char* qmlUrl, int major, int minor); }
+
+#define DEFINE_ENUM_REGISTRATION_FUNCTION(NAMESPACE) \
+    void NAMESPACE##Enums::registerEnumTypes(const char *qmlUrl, int major, int minor)
+
+#define REGISTER_ENUM_CONTAINER(NAME) \
+    qRegisterMetaType<NAME>(#NAME); \
+    qRegisterMetaType<NAME::Enum>(#NAME "::Enum"); \
+    qRegisterMetaType<QList<NAME::Enum>>("QList<" #NAME "::Enum>"); \
+    qRegisterMetaType<QList<NAME>>("QList<" #NAME ">"); \
+    NAME::registerToQml(qmlUrl, major, minor);
+
+#define REGISTER_ENUMS(NAMESPACE, URI, MAJOR, MINOR) \
+    NAMESPACE##Enums::registerEnumTypes(URI, MAJOR, MINOR);
+
+#endif // ENUMCONTAINER_H
