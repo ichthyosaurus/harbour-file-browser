5f651cb0e1caeb4c4ef1b2cfad17d21aa728c1cd
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-08-11 20:51:30 +0200

    Link to Qt docs when translating date formats

--- src/globals.cpp
+++ src/globals.cpp
@@ -118,12 +118,13 @@ QString datetimeToString(QDateTime datetime, bool longFormat)
     // return time for today or date+time for older
     // include more information if 'longFormat' is true
     if (datetime.date() == QDate::currentDate()) {
-        return datetime.toString(QObject::tr("hh:mm:ss"));
+        return datetime.toString(
+            QObject::tr("hh:mm:ss", "Time format. Use what's common in your language. See https://doc.qt.io/archives/qt-5.15/qml-qtqml-date.html#format-strings for details."));
     } else {
         if (longFormat) {
-            return datetime.toString(QObject::tr("dd MMM yyyy, hh:mm:ss t"));
+            return datetime.toString(QObject::tr("dd MMM yyyy, hh:mm:ss t", "Date and time format. Use what's common in your language. See https://doc.qt.io/archives/qt-5.15/qml-qtqml-date.html#format-strings for details."));
         } else {
-            return datetime.toString(QObject::tr("dd.MM.yy, hh:mm"));
+            return datetime.toString(QObject::tr("dd.MM.yy, hh:mm", "Date and format. Use what's common in your language. See https://doc.qt.io/archives/qt-5.15/qml-qtqml-date.html#format-strings for details."));
         }
     }
 }
